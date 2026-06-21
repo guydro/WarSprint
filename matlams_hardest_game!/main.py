12e8a11eb66dbdaeced47a947ccf7e20b634795a
guydro <guydror500@gmail.com>
2026-06-22 00:48:42 +0300

    changed things

--- matlams_hardest_game!/main.py
+++ matlams_hardest_game!/main.py
@@ -431,8 +431,8 @@ import struct
 # Hardcoded settings
 # =========================
 
-IMAGE_PATH = r"C:\Users\TLP\OneDrive\Desktop\sprint4\convertico-abstract-design-4-bit-16-colors-bmp.bmp"
-BATCHES_OUTPUT_PATH = r"C:\Users\TLP\Desktop\encoded_batches.txt"
+IMAGE_PATH = r"..\convertico-abstract-design-4-bit-16-colors-bmp.bmp"
+BATCHES_OUTPUT_PATH = r"..\encoded_batches.txt"
 
 BATCHSIZE = batch_size - 3
 
